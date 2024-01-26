bfd70a7ac7f94e7c3a49c59000f9e7c7e5cd6b16
ATLANT176 <danila2726@mail.ru>
2024-01-26 15:26:50 +0300

    final step

---
+++ bin/brain-prime.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync'
+import { greetUser } from '../src/cli.js'
+import { gameStart, lose } from '../src/index.js'
+
+function checkPrime(number) {
+    let isPrime = true;
+    for (let i = 2; i < Math.sqrt(number); i += 1) {
+        if (number % i === 0) {
+            isPrime = false;
+            break
+        } 
+    }
+    return isPrime ? 'yes' : 'no'
+}
+
+const generateMaxNumber = () => {
+        return Math.floor(Math.random() * 200)
+    }
+
+const brainPrime = () => {
+    const randomNumber = generateMaxNumber()
+    console.log(`Question: ${randomNumber}`)
+    const correctAnswer = checkPrime(randomNumber)
+    const userAnswer = readlineSync.question('Your answer: ')
+    if (correctAnswer === userAnswer) {
+        return 1
+    } else {
+        lose(userAnswer, correctAnswer, name)
+        return 0
+    }
+}
+
+const gameName = brainPrime
+const name = greetUser()
+gameStart(name, gameName, 3)
