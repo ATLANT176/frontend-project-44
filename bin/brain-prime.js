024054c596003654f1103334ae80882bfcaf92a3
ATLANT176 <danila2726@mail.ru>
2024-01-26 18:07:57 +0300

    FINAL

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -20,6 +20,7 @@ const generateMaxNumber = () => {
 
 const brainPrime = () => {
     const randomNumber = generateMaxNumber()
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no".')
     console.log(`Question: ${randomNumber}`)
     const correctAnswer = checkPrime(randomNumber)
     const userAnswer = readlineSync.question('Your answer: ')
